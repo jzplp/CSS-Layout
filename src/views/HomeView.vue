36794b69f1a525dd5f21104e995f742d4b41261d
jzplp <jzplp@qq.com>
2023-10-21 14:08:04 +0800

    项目初始化，创建第一个布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
+import type { Menu } from '@/types/menus'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+
+const menus: Array<Menu> = [
+  {
+    name: '左右三栏布局',
+    desc: '使用flex',
+    path: 'flex/ThreeColumn'
+  }
+]
+
+function clickMenu(item: Menu) {
+  router.push(item.path)
+}
+
 </script>
 
 <template>
   <div>
-    你好
+    <div v-for="item in menus" :key="item.path" class="menu">
+        <div class="menuTitle" @click="clickMenu(item)"> {{ item.name }} </div>
+        <div> {{ item.desc || '-' }} </div>
+      </div>
   </div>
 </template>
 
 <style>
+.menu {
+  margin: 10px;
+}
+.menuTitle {
+  color: blue;
+  text-decoration: underline;
+  cursor:pointer;
+}
 </style>
---
+++ src/views/flex/ThreeColumn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="whole-page container">
+    <div class="left">左侧</div>
+    <div class="middle">中间</div>
+    <div class="right">右侧</div>
+  </div>
+</template>
+
+<style scoped>
+.whole-page {
+  height: 100vh;
+  width: 100vw;
+}
+.container {
+  display: flex;
+  .left {
+    width: 400px;
+    background-color: pink;
+  }
+  .right {
+    width: 300px;
+    background-color: green;
+  }
+  .middle {
+    flex:1;
+    background-color: yellow;
+  }
+}
+</style>
