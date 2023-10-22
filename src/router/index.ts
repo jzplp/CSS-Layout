86bf34e714c3ca3f85d4aecadd34970c8827d808
jzplp <jzplp@qq.com>
2023-10-22 17:13:03 +0800

    修改路由为hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from '~pages'
 import HomeView from '@/views/HomeView.vue'
 
@@ -9,7 +9,7 @@ routes.push({
 })
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
